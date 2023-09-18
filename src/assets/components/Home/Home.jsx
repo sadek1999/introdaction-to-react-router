fa06f5259cec6347fb12e435730d5630962a6f32
Sadek Ali <sadekali1530@gmail.com>
2023-09-18 21:49:00 +0600

    add the code

---
+++ src/assets/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Contact us </h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/assets/components/contact/Contact.jsx
@@ -0,0 +1,11 @@
+
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>for contact use call in number</h1>
+        </div>
+    );
+};
+
+export default Contact;
